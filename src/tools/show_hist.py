ba2cc4d030110eb1ff3475f276f77145ac419072
tktbtk <tktbtk@hongo.wide.ad.jp>
2017-04-17 13:11:47 -0400

    int graph

--- src/tools/show_hist.py
+++ src/tools/show_hist.py
@@ -4,6 +4,7 @@ matplotlib.use("AGG")
 from matplotlib import pyplot as plt
 import sys
 import numpy as np
+import matplotlib.ticker as ticker
 
 with open(sys.argv[1], "rb") as f:
     path_length = pickle.load(f)
@@ -20,6 +21,7 @@ y_d /= np.sum(y_d)
 plt.bar(path_length["announce"].keys(), y_w, width = 1.0, edgecolor = "red", label = "update" , fill=False, lw=2)
 plt.bar(path_length["duplicate"].keys(), y_d, width = 1.0, edgecolor = "blue", label = "duplicate announce", fill=False ,lw = 2)
 lgnd = plt.legend(bbox_to_anchor=(1.05, 0), loc='lower left', borderaxespad=0)
+plt.gca().get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
 plt.title("AS Path Length in withdraw and in duplicate withdraw")
 plt.xlabel("AS Path Length")
 plt.ylabel("the number of messages")
